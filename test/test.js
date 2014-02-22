9959a18cf131a51d589d020e14375adf36aecdd1
David Björklund <david.bjorklund@gmail.com>
2014-02-22 14:37:59 +0100

    test: use identifyLicence-name

--- test/test.js
+++ test/test.js
@@ -1,67 +1,67 @@
 var test = require('tap').test
 
-	,	licenseFind = require('../identify-licence')
+	,	identifyLicence = require('../identify-licence')
 
 test('with GPL text', function (t) {
 
-	t.deepEqual(licenseFind('blah GPL blah'), ['GPL'])
-	t.deepEqual(licenseFind('GPL blah'), ['GPL'])
+	t.deepEqual(identifyLicence('blah GPL blah'), ['GPL'])
+	t.deepEqual(identifyLicence('GPL blah'), ['GPL'])
 	t.end()
 })
 
 test('with LGPL text', function (t) {
 
-	t.deepEqual(licenseFind('blah LGPL blah'), ['LGPL'])
-	t.deepEqual(licenseFind('LGPL blah'), ['LGPL'])
+	t.deepEqual(identifyLicence('blah LGPL blah'), ['LGPL'])
+	t.deepEqual(identifyLicence('LGPL blah'), ['LGPL'])
 	t.end()
 })
 
 test('with GPLvx text', function (t) {
 
-	t.deepEqual(licenseFind('blah GPLv2 blah'), ['GPL'])
-	t.deepEqual(licenseFind('blah GPLv9 blah'), ['GPL'])
-	t.deepEqual(licenseFind('GPLv2 blah'), ['GPL'])
+	t.deepEqual(identifyLicence('blah GPLv2 blah'), ['GPL'])
+	t.deepEqual(identifyLicence('blah GPLv9 blah'), ['GPL'])
+	t.deepEqual(identifyLicence('GPLv2 blah'), ['GPL'])
 	t.end()
 })
 
 test('with MIT text', function (t) {
 
-	t.deepEqual(licenseFind('blah MIT blah'), ['MIT'])
-	t.deepEqual(licenseFind('MIT blah'), ['MIT'])
+	t.deepEqual(identifyLicence('blah MIT blah'), ['MIT'])
+	t.deepEqual(identifyLicence('MIT blah'), ['MIT'])
 	t.end()
 })
 
 test('with (MIT) text', function (t) {
 
-	t.deepEqual(licenseFind('blah (MIT) blah'), ['MIT'])
-	t.deepEqual(licenseFind('(MIT) blah'), ['MIT'])
+	t.deepEqual(identifyLicence('blah (MIT) blah'), ['MIT'])
+	t.deepEqual(identifyLicence('(MIT) blah'), ['MIT'])
 	t.end()
 })
 
 test('with MPL text', function (t) {
-	t.deepEqual(licenseFind('blah MPL blah'), ['MPL'])
-	t.deepEqual(licenseFind('MPL blah'), ['MPL'])
+	t.deepEqual(identifyLicence('blah MPL blah'), ['MPL'])
+	t.deepEqual(identifyLicence('MPL blah'), ['MPL'])
 	t.end()
 })
 
 test('with Apache License text', function (t) {
-	t.deepEqual(licenseFind('blah Apache\nLicense blah'), ['Apache'])
-	t.deepEqual(licenseFind('Apache\nLicense blah'), ['Apache'])
+	t.deepEqual(identifyLicence('blah Apache\nLicense blah'), ['Apache'])
+	t.deepEqual(identifyLicence('Apache\nLicense blah'), ['Apache'])
 	t.end()
 })
 
 test('with DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE text', function (t) {
-	t.deepEqual(licenseFind('blah DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE blah'), ['WTFPL'])
-	t.deepEqual(licenseFind('DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE blah'), ['WTFPL'])
-	t.deepEqual(licenseFind('dO WHAT the fUck you want tO PUBLIC licensE blah'), ['WTFPL'])
-	t.deepEqual(licenseFind('blah DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE blah'), ['WTFPL'])
+	t.deepEqual(identifyLicence('blah DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE blah'), ['WTFPL'])
+	t.deepEqual(identifyLicence('DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE blah'), ['WTFPL'])
+	t.deepEqual(identifyLicence('dO WHAT the fUck you want tO PUBLIC licensE blah'), ['WTFPL'])
+	t.deepEqual(identifyLicence('blah DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE blah'), ['WTFPL'])
 	t.end()
 })
 
 test('with ISC text', function (t) {
 
-	t.deepEqual(licenseFind('blah ISC blah'), ['ISC'])
-	t.deepEqual(licenseFind('ISC blah'), ['ISC'])
+	t.deepEqual(identifyLicence('blah ISC blah'), ['ISC'])
+	t.deepEqual(identifyLicence('ISC blah'), ['ISC'])
 	t.end()
 })
 
@@ -69,16 +69,16 @@ test('with Eclipse Public License text', function (t) {
 
 	t.test('in full', function (t) {
 
-		t.deepEqual(licenseFind('blah EPL blah'), ['Eclipse Public License'])
+		t.deepEqual(identifyLicence('blah EPL blah'), ['Eclipse Public License'])
 		t.end()
 	})
 
 	t.test('in abbreviation', function (t) {
 
-		t.deepEqual(licenseFind('blah EPL blah'), ['Eclipse Public License'])
-		t.deepEqual(licenseFind('blah EPL-1.0 blah'), ['Eclipse Public License'])
-		t.deepEqual(licenseFind('EPL blah'), ['Eclipse Public License'])
-		t.deepEqual(licenseFind('EPL-1.0 blah'), ['Eclipse Public License'])
+		t.deepEqual(identifyLicence('blah EPL blah'), ['Eclipse Public License'])
+		t.deepEqual(identifyLicence('blah EPL-1.0 blah'), ['Eclipse Public License'])
+		t.deepEqual(identifyLicence('EPL blah'), ['Eclipse Public License'])
+		t.deepEqual(identifyLicence('EPL-1.0 blah'), ['Eclipse Public License'])
 		t.end()
 	})
 
@@ -87,13 +87,13 @@ test('with Eclipse Public License text', function (t) {
 
 test('with BSD text', function (t) {
 
-	t.deepEqual(licenseFind('blah BSD blah'), ['BSD'])
-	t.deepEqual(licenseFind('BSD blah'), ['BSD'])
+	t.deepEqual(identifyLicence('blah BSD blah'), ['BSD'])
+	t.deepEqual(identifyLicence('BSD blah'), ['BSD'])
 	t.end()
 })
 
 test('with dual license, e.g. GPL & MIT text', function (t) {
 
-	t.deepEqual(licenseFind('blah MIT blah\n\ncats GPL cats'), ['GPL', 'MIT'])
+	t.deepEqual(identifyLicence('blah MIT blah\n\ncats GPL cats'), ['GPL', 'MIT'])
 	t.end()
 })
